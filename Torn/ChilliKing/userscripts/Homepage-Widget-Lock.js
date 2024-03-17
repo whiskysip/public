c69a4a7277297d62887ca8e6bfeb00d491250d65
whiskysip <29978054+whiskysip@users.noreply.github.com>
2024-03-17 18:49:32 +0000

    Add files via upload

--- Torn/ChilliKing/userscripts/Homepage-Widget-Lock.js
+++ Torn/ChilliKing/userscripts/Homepage-Widget-Lock.js
@@ -1,1 +1,25 @@
+// ==UserScript==
+// @name         Homepage Widget Lock test
+// @namespace    http://tampermonkey.net/
+// @version      2024-03-17
+// @description  Prevents the widgets so they can't be moved by accident by removing the move button
+// @author       ChilliKing
+// @match        https://www.torn.com/index.php
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+
+    // Set this variable to true to enable the removal of accordion-header-move elements
+    var removeAccordions = true;
+
+    window.addEventListener('load', function() {
+        if (removeAccordions) {
+            var elements = document.querySelectorAll('.accordion-header-move');
+            elements.forEach(function(element) {
+                element.parentNode.removeChild(element);
+            });
+        }
+    });
+})();
